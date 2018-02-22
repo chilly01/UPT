8914e7cf2c0fb8b0bd20d7398a3cd2238a1b9efe
Cody Hillyard <codyhillyard@gmail.com>
2018-02-22 08:27:16 -0700

    removed user from menu

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :microposts
   has_many :tours
   validates_presence_of :name
-  validates_presence_of :email
+  validates :email,:presence => true, uniqueness: { case_sensitive: false }
   validates :password, :length => { :minimum => 7 }
   has_secure_password
 end
