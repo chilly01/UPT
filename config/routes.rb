71ff56ae548f6783fc606d62590aa17e48b03f5f
Cody Hillyard <codyhillyard@gmail.com>
2018-03-18 22:28:58 -0600

    removing rails interface for roles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :roles
+  get    '/roles',   to: 'roles#index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get    '/logout',  to: 'sessions#destroy'
